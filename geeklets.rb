51773e66b4d238a8ccd5477bb3306fb3956ab6f2
Robert Jorgenson <rjorgenson@gmail.com>
2010-04-20 22:28:23 -0700

    added time remaining to battery script

--- geeklets.rb
+++ geeklets.rb
@@ -5,11 +5,28 @@ def get_time_date # gets the current time and formats it as specified
 end
 
 def get_battery_info # gets the battery info on macbooks/pros
-  chg = `ioreg -l | grep ExternalConnected | awk '{ print $5 }'` # is battery charged
+  chrg = `ioreg -l | grep ExternalConnected | awk '{ print $5 }'` # is battery charged
+  timel = `ioreg -l | grep TimeRemaining | awk '{ print $5 }'` # time remaining on battery
+  # display time remaining on battery or charging if plugged in
+  if chrg.strip == "Yes" then
+    time = "Charging"
+  else
+    if timel.strip == "0" then
+      time = "Calculating..."
+    else
+      hour = timel.strip.to_i / 60
+      min = timel.strip.to_i - (hour * 60)
+      # fix minutes to be 2 digits long always
+      if min < 10 then
+        min = "0#{min}"
+      end
+      time = "#{hour}:#{min}"
+    end
+  end
   mb = `ioreg -l | grep MaxCapacity | awk '{ print $5 }'` # maximum capacity
   cb = `ioreg -l | grep CurrentCapacity | awk '{ print $5 }'` # current capacity
   # determine battery % and round off to match system battery %
-  puts "#{(chg.strip == "Yes" ? "Charging: " : "Battery: ")}#{(cb.to_f / mb.to_f * 100).round}%"
+  puts "Battery: #{(cb.to_f / mb.to_f * 100).round}% (#{time})"
 end
 
 # determine which script to execute
